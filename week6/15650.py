7a4143d32c491c371ce13cdd6e532a14a1cd0f44
Choseoyoung <seoyoung0000@naver.com>
2022-05-18 10:36:12 +0900

    Add week6/15650

---
+++ week6/15650.py
@@ -0,0 +1,34 @@
+# 15650, N과 M (2), Silver 3
+# https://www.acmicpc.net/problem/15650
+
+import sys
+
+# 초기 변수 설정
+N1, N2 = map(int, sys.stdin.readline().split())
+arr = [i for i in range(1, N1+1,1)]
+N = 1
+for i in range(N1,N1-N2,-1):
+    N*=i
+for i in range(1,N2+1,1):
+    N=N//i
+
+result=[]
+def N_M(i,S):
+    global result
+
+    if(len(result)!=N):
+        if(len(S)==N2):
+            result.append(S)
+            N_M(i+1, [])
+        else:
+            for j in range(i, len(arr), 1):
+                if arr[j] not in S:
+                    N_M(j+1,S+[arr[j]])
+    else:
+        return result
+
+N_M(0,[])
+for a in result:
+    for aa in a[:-1:]:
+        print(aa,end=" ")
+    print(a[-1])
