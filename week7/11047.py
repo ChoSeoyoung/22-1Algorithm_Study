386c05d089ace1d5a21be3007e79d10d0c08d9b7
Choseoyoung <seoyoung0000@naver.com>
2022-05-28 22:12:04 +0900

    Edit week7/11047

--- week7/11047.py
+++ week7/11047.py
@@ -4,25 +4,27 @@
 import sys
 
 N, amount = map(int, sys.stdin.readline().split())
-S = sorted([int(sys.stdin.readline()) for _ in range(N)])
+S = list(reversed([int(sys.stdin.readline()) for _ in range(N)]))
 
 def greedy(S, amount):
     total_cnt=0
     cur_amt=0
+    idx=-1
 
     while True:
         #선택 과정
-        tmp=S.pop()
+        idx+=1
+        tmp=S[idx]
         
         #적절성 확인
-        while(tmp+cur_amt<=amount):
-            total_cnt+=1 
-            cur_amt+=tmp
+        if(tmp+cur_amt<=amount):
+            cur_cnt=((amount-cur_amt)//tmp) 
+            total_cnt+=cur_cnt
+            cur_amt+=tmp*cur_cnt
 
         #해답 검사
         if(cur_amt==amount):
             break
-
 
     return total_cnt
 
