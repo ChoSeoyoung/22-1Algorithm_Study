73c98dfcc3a973dea288e757b7856102f3b32992
Choseoyoung <seoyoung0000@naver.com>
2022-06-20 14:13:32 +0900

    1000 1001 Bronze 5

---
+++ 1000.py
@@ -0,0 +1,7 @@
+# 1000
+# https://www.acmicpc.net/problem/1000
+
+import sys
+
+N1, N2 = map(int, sys.stdin.readline().split())
+sys.stdout.write(str(N1+N2))
---
+++ 1001.py
@@ -0,0 +1,4 @@
+import sys
+
+N1, N2 = map(int, sys.stdin.readline().split())
+sys.stdout.write(str(N1-N2))
