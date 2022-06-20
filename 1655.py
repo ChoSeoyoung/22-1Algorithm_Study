d6aeae3d5fbcc98f4e08d131b2214814994946fe
Choseoyoung <seoyoung0000@naver.com>
2022-06-20 14:05:48 +0900

    Edit 1655 Gold 2

--- 1655.py
+++ 1655.py
@@ -3,85 +3,89 @@
 # -1 (0 1)/ 2 -1 1 2
 import sys
 from queue import PriorityQueue
+import heapq
 
 N=int(sys.stdin.readline())
 llist=[int(sys.stdin.readline()) for _ in range(N)]
-max_Q=PriorityQueue() #small Q
-min_Q=PriorityQueue() #big Q
-
+max_Q=[] #small Q
+min_Q=[] #big Q
 
 for num in llist:
-    if min_Q.qsize()==0 and max_Q.qsize()==0:
+    if len(min_Q)==0 and len(max_Q)==0:
         mid_num=num
-        max_Q.put(num*(-1))
+        heapq.heappush(max_Q,num*(-1))
         sys.stdout.write(str(mid_num))
         sys.stdout.write('\n')
     else:
-        if max_Q.qsize()!=0 and min_Q.qsize()==0:
-            max_num=(max_Q.get()*-1)
+        if len(max_Q)!=0 and len(min_Q)==0:
+            max_num=(heapq.heappop(max_Q)*-1)
             if num<=max_num:
                 mid_num=num
                 sys.stdout.write(str(mid_num))
                 sys.stdout.write('\n')
-                max_Q.put(mid_num*-1)
-                min_Q.put(max_num)
+                heapq.heappush(max_Q,mid_num*-1)
+                heapq.heappush(min_Q, max_num)
             elif num>max_num:
                 mid_num=max_num
                 sys.stdout.write(str(mid_num))
                 sys.stdout.write('\n')
-                max_Q.put(max_num*-1)
-                min_Q.put(num)
-        elif max_Q.qsize()==0 and min_Q.qsize()!=0:
-            min_num=min_Q.get()
+                heapq.heappush(max_Q, max_num*-1)
+                heapq.heappush(min_Q, num)
+        elif len(max_Q)==0 and len(min_Q)!=0:
+            #min_num=min_Q[0]
+            min_num=heapq.heappop(min_Q)
             if num>=min_num:
                 mid_num=min_num
                 sys.stdout.write(str(mid_num))
                 sys.stdout.write('\n')
-                max_Q.put(min_num*-1)
-                min_Q.put(mid_num)
+                heapq.heappush(max_Q, min_num*-1)
+                heapq.heappush(min_Q, mid_num)
             elif num<min_num:
                 mid_num=num
                 sys.stdout.write(str(mid_num))
                 sys.stdout.write('\n')
-                max_Q.put(mid_num*-1)
-                min_Q.put(max_num)
-        elif max_Q.qsize()==min_Q.qsize() and max_Q.qsize()!=0 and min_Q.qsize()!=0:
-            max_num=max_Q.get()*-1
-            min_num=min_Q.get()
+                heapq.heappush(max_Q, mid_num*-1)
+                heapq.heappush(min_Q, max_num)
+        elif len(max_Q)==len(min_Q) and len(max_Q)!=0 and len(min_Q)!=0:
+            max_num=max_Q[0]*-1
+            min_num=min_Q[0]
+            #max_num=heapq.heappop(max_Q)*-1
+            #min_num=heapq.heappop(min_Q)
             if num<=max_num:
-                max_Q.put(num*-1)
-                max_Q.put(max_num*-1)
-                min_Q.put(min_num)
+                heapq.heappush(max_Q, num*-1)
+                #heapq.heappush(max_Q, max_num*-1)
+                #heapq.heappush(min_Q, min_num)
             elif max_num<num and num<min_num:
-                max_Q.put(max_num*-1)
-                max_Q.put(num*-1)
-                min_Q.put(min_num)
+                #heapq.heappush(max_Q, max_num*-1)
+                heapq.heappush(max_Q, num*-1)
+                #heapq.heappush(min_Q,min_num)
             else:
-                max_Q.put(max_num*-1)
-                max_Q.put(min_num*-1)
-                min_Q.put(num)
-            mid_num=max_Q.get()*-1
-            sys.stdout.write(str(mid_num))
+                #heapq.heappush(max_Q, max_num*-1)
+                min_num=heapq.heappop(min_Q)
+                heapq.heappush(max_Q, min_num*-1)
+                heapq.heappush(min_Q, num)
+            sys.stdout.write(str(max_Q[0]*-1))
             sys.stdout.write('\n')
-            max_Q.put(mid_num*-1)
-        elif max_Q.qsize()>min_Q.qsize() and max_Q.qsize()!=0 and min_Q.qsize()!=0:
+        elif len(max_Q)>len(min_Q)and len(max_Q)!=0 and len(min_Q)!=0:
             # 1 2/ 5   10, 2 5 10
-            max_num=max_Q.get()*-1
-            min_num=min_Q.get()
+            max_num=max_Q[0]*-1
+            min_num=min_Q[0]
+            #max_num=heapq.heappop(max_Q)*-1
+            #min_num=heapq.heappop(min_Q)
             if num<=max_num:
-                max_Q.put(num*-1)
-                min_Q.put(max_num)
-                min_Q.put(min_num)
+                max_num=heapq.heappop(max_Q)*-1
+                min_num=heapq.heappop(min_Q)
+                heapq.heappush(max_Q, num*-1)
+                heapq.heappush(min_Q, max_num)
+                heapq.heappush(min_Q, min_num)
             elif max_num<num and num<min_num:
                 # max num min num num
-                max_Q.put(max_num*-1)
-                min_Q.put(num)
-                min_Q.put(min_num)
+                #heapq.heappush(max_Q, max_num*-1)
+                heapq.heappush(min_Q, num)
+                #heapq.heappush(min_Q, min_num)
             else: # max num min num num
-                max_Q.put(max_num*-1)
-                min_Q.put(min_num)
-                min_Q.put(num)
-            mid_num=max_Q.get()*-1
-            sys.stdout.write(str(mid_num))
+                #heapq.heappush(max_Q, max_num*-1)
+                #heapq.heappush(min_Q, min_num)
+                heapq.heappush(min_Q, num)
+            sys.stdout.write(str(max_Q[0]*-1))
             sys.stdout.write('\n')
-            max_Q.put(mid_num*-1)
